9859a9606191c587999a35dd2347823064e22fe3
Di M <itsdianamez@gmail.com>
2025-12-21 13:47:26 +0300

    Техническое задание проекта №14

---
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -0,0 +1,23 @@
+package ru.practicum.shareit.user;
+
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+
+public class UserMapper {
+
+    public static UserDto toUserDto(User user) {
+        return new UserDto(
+                user.getId(),
+                user.getName(),
+                user.getEmail()
+        );
+    }
+
+    public static User toUser(UserDto dto) {
+        return new User(
+                dto.getId(),
+                dto.getName(),
+                dto.getEmail()
+        );
+    }
+}
